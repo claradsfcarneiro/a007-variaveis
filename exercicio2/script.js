a4e75556ddb99267b808e76455c537736b86bac5
Clara <claradsfcarneiro@gmail.com>
2022-08-29 20:50:31 -0300

    Realizar exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+/* const respostaA = String(prompt("Você gosta de música pop?"));
+const respostaB = String(prompt("Você gosta da Taylor Swift?"));
+const respostaC = String(prompt("Você irá no show dela se ela vier no Brasil?"));
+
+console.log(respostaA);
+console.log(respostaB);
+console.log(respostaC); */ 
+
+const perguntaA = "Você gosta de música pop?";
+const respostaA = prompt(perguntaA);
+const perguntaB = "Você gosta da Taylor Swift?";
+const respostaB = prompt(perguntaB);
+const perguntaC = "Você irá no show dela se ela vier ao Brasil?";
+const respostaC = prompt(perguntaC);
+
+console.log(`${perguntaA} - ${respostaA}`);
+console.log(`${perguntaB} - ${respostaB}`);
+console.log(`${perguntaC} - ${respostaC}`);
